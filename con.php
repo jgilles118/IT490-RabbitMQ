3ef63993310c7c79cfd92db3b10a837ad08a8c6c
jgilles118 <35711210+jgilles118@users.noreply.github.com>
2018-03-14 11:44:32 -0400

    Update con.php

--- con.php
+++ con.php
@@ -9,11 +9,10 @@ ini_set('display_errors', 1);
 include("djmvClient.php");
 include("vmdajClient.php");
 
-//From blog:w
-i.html
-$u =$_GET['User'];
-$p =$_GET['Pass'];
-$type =$_GET['type'];
+//From blog.html
+$u =$_POST['User'];
+$p =$_POST['Pass'];
+$type =$_POST['type'];
 
 //DB entrance
 $host = "localhost";
@@ -23,25 +22,25 @@ $dbase = "userlogin";
 
 if($type == "Login")    {
 
-        header("refresh:1; url = 'djmvClient.php'");
-
         //Session incase user,password,type needed elsewhere 
         $_SESSION["username"] = $u;
         $_SESSION["password"] = $p;
-        $_SESSION["type"] = $type;	}
+        $_SESSION["type"] = $type;
+	 header("refresh:1; url = 'djmvClient.php'"); }
 
 $con = mysqli_connect($host, $user, $pass, $dbase);
 
         $q = mysqli_query($con, "SELECT * from user WHERE Username = '$u' && Password='$p'");
         $rows = mysqli_num_rows($q);
-                if($rows==1) {  $_SESSION["message"]= "Access GRANTED";
-                                header("refresh:1; url = 'vmdajClient.php'");
-				header("refresh:1; url = 'http://api2.bigoven.com'");    }
+
+        if($rows==1) {  $_SESSION["message"]= "Access GRANTED";
+                         header("refresh:1; url = 'vmdajClient.php'");
+			header("refresh:1; url = 'http://api2.bigoven.com'");    }
                                 
-		else {	 $_SESSION["message"]= "Not A Member";
-                        header("refresh:1; url = 'djmvClient.php'"); 
-                        $url = "register.html";
-                        header ("refresh:2; url =$url");  }
+	else {	 $_SESSION["message"]= "Not A Member";
+                 header("refresh:1; url = 'djmvClient.php'"); 
+                 $url = "register.html";
+                 header ("refresh:2; url =$url");  }
 
 
 
